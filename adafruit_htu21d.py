ff5166feb02a5a43790e4d4bba3b53254e6dcf09
Kevin Townsend <contact@microbuilder.eu>
2018-09-14 19:30:47 +0200

    Naming fix

--- adafruit_htu21d.py
+++ adafruit_htu21d.py
@@ -19,8 +19,9 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
 """
-`adafruit_HTU21D`
+`adafruit_htu21d`
 ====================================================
 
 This is a breakout for the Adafruit HTU21D-F humidity sensor breakout.
